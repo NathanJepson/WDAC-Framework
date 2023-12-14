f8bfc2250bb2c90a6e4996c3b9e22c63e83c5331
NathanJepson <61210670+NathanJepson@users.noreply.github.com>
2023-12-14 15:53:06 -0700

    Removed redundant logging-out of users, except when RestartForce is used.

--- Resources/Restart-WDACDevices.ps1
+++ Resources/Restart-WDACDevices.ps1
@@ -7,39 +7,5 @@ function Restart-WDACDevices {
         [string[]]$Devices
     )
 
-    $scriptLogOutUsers = {
-        
-        #Log out every user
-                
-        ## Find all sessions matching the specified username
-        $sessions = quser
-
-        #Take out the Headers
-        $sessions = $sessions[1..($sessions.count-1)]
-
-        ## Parse the session IDs from the output
-        $sessionIds = @()
-        for ($i=0; $i -lt $sessions.Count; $i++) {
-            $sessionIds += ($sessions[$i] -split ' +')[2]
-        }
-
-        ## Loop through each session ID and pass each to the logoff command
-        $sessionIds | ForEach-Object {
-            try {
-                Start-Process logoff -ArgumentList "$_" -Wait -ErrorAction Stop
-            } catch {
-                if ($_.Exception.Message -match 'No user exists') {
-                    #The user is not logged in
-                    continue
-                } else {
-                    throw $_
-                }
-            }
-        }
-        
-        Start-Sleep -Seconds 14
-    }
-
-    Invoke-Command -ComputerName $Devices -ScriptBlock $scriptLogOutUsers -ErrorAction SilentlyContinue
     Restart-Computer -ComputerName $Devices -Force -ErrorAction SilentlyContinue
 }
